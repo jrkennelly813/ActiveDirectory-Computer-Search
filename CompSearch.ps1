377897396d5a94017047297bcee008dfec6b143e
JR-CodeHub <71028103+jrkennelly813@users.noreply.github.com>
2020-11-09 10:52:51 -0700

    Update CompSearch.ps1
    Added Search Filter comboBox,
    Continued work on functionality..

--- CompSearch.ps1
+++ CompSearch.ps1
@@ -3,11 +3,15 @@ Add-Type -AssemblyName System.Windows.Forms
 Add-Type -AssemblyName System.Drawing
 
 #Region Functions
-Function SearchAD ($Asset) {
-    if ($Asset -Match "^\d{6}$") {
-        $Computer = Get-ADComputer -Filter ('Name -Like "*{0}*"' -f $Asset)
+Function SearchAssetTag ($Asset) {
+    If ($Asset -Match "^\d{6}$") {
+        $Computer = Get-ADComputer -Filter ('Name -Like "*'+ $Asset +'*"')
+        return $Computer
     }
-    return $Computer
+    Else {
+        return 'No Computer Found'
+    }
+    
 }
 #EndRegion 
 #Region UI
@@ -16,10 +20,11 @@ $Form.Text = "Active Directory Computer Search"
 $Form.StartPosition = 'CenterScreen'
 $Form.ClientSize = '225, 150'
 
-$Label = New-Object System.Windows.Forms.Label
-$Label.Text = "Search device asset tag:"
-$Label.AutoSize = $True
-$Label.Location = New-Object System.Drawing.Point(20, 20)
+$SearchBase = New-Object System.Windows.Forms.ComboBox
+$SearchBase.Text = "Search Filter"
+$SearchBase.AutoSize = $True
+$SearchBase.Location = New-Object System.Drawing.Point(20, 15)
+$SearchBase.Items.AddRange(@('Asset Tag #', 'Building', 'Room', 'OU'))  
 
 $SearchField = New-Object System.Windows.Forms.TextBox
 $SearchField.Text = ""
@@ -37,15 +42,20 @@ $ComputerInfo.ScrollBars = 'Both'
 $ComputerInfo.Size = New-Object System.Drawing.Point(180, 65)
 $ComputerInfo.Location = New-Object System.Drawing.Point(20, 75)
 
-$Form.Controls.AddRange(@($Label, $SearchField, $SearchButton, $ComputerInfo))
+$Form.Controls.AddRange(@($SearchBase, $SearchField, $SearchButton, $ComputerInfo))
 #EndRegion
 #Region UI Events
 $SearchButton.Add_Click({
-    $Info = SearchAD -Asset $SearchField.Text
-    $Info = $Info.toString().split(',')
-    ForEach ($_ in $Info) {
-        $ComputerInfo.Text += $_ + [System.Environment]::NewLine
+    If ($SearchBase.Text -eq 'Asset Tag #') {
+        $Info = SearchAssetTag -Asset $SearchField.Text
+        try {
+            $ComputerInfo.Text = $Info.toString().split(',')
+        }
+        catch {
+            $ComputerInfo.Text = 'Computer cannot be found'
+        }
     }
+
 })
 
 [void]$Form.ShowDialog()
